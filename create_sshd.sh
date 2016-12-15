bbae28be45cbcf3fa97dc6539af0e9e9cfd307b0
raoulshih <raoul.shih@gmail.com>
2016-12-15 05:32:27 +0000

    Calling docker compose to create sshd container

---
+++ create_sshd.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+if [ $# -ne 3 ]  ; then
+    echo "usage: create_ssh.sh client_name client_port ssh_root_password"
+else
+    export CLIENT_ID=$1
+    export PUBLIC_HOST_PORT=$2
+    export SSH_ROOT_PASSWORD=$3
+    echo "Prepare to create ssh container CLIENT_ID={$CLIENT_ID}, HOST_PORT={$PUBLIC_HOST_PORT}, SSH_ROOT_PASS={$SSH_ROOT_PASSWORD}"
+    docker-compose run reversesshpublic
+fi
+
